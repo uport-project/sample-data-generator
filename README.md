321b19b4cdf33f3f219b6e845a20d9e555851fb8
Simonas Karuzas <simonas@not.cat>
2018-11-07 15:19:11 +0200

    MVP version of tables and views

---
+++ README.md
@@ -0,0 +1,47 @@
+# Dummy user data generator
+
+This script creates sqlite database with fake signed data.
+
+First it creates specified number of ethr-did identities, and then uses them to sign data (`firstName`, `lastName`, `avatar` and `email`) about each other.
+
+## Usage
+
+```
+npm run start -- --dids 15
+
+Creating identities...
+Done.
+Signing claims...
+Done.
+Creating tables and views
+Done.
+Inserting values...
+Done.
+
+SELECT count(*) FROM signed_data
+
+count(*)
+--------
+900
+
+
+SELECT * FROM profiles
+
+sub                                                  firstName  lastName    avatar
+---------------------------------------------------  ---------  ----------  ----------------------------------------------------------------------
+did:ethr:0x03230b1d564655140174a39f519ce231ac43d7bf  Mohammad   Hansen      https://s3.amazonaws.com/uifaces/faces/twitter/kimcool/128.jpg
+did:ethr:0x1decc84ef70ab3d1bde64f533a898f8eadab3b95  Ernie      Welch       https://s3.amazonaws.com/uifaces/faces/twitter/jayphen/128.jpg
+did:ethr:0x26d8b40882263d2a1cf8bb4feaa58ab5e54b359b  Albertha   Shanahan    https://s3.amazonaws.com/uifaces/faces/twitter/newbrushes/128.jpg
+did:ethr:0x27d3d0ba6e5af2168fe7bf4121d8297d48220932  Justus     Herzog      https://s3.amazonaws.com/uifaces/faces/twitter/ralph_lam/128.jpg
+did:ethr:0x2f52448dd010d2157c953bc37779891afff2980c  Ora        Ernser      https://s3.amazonaws.com/uifaces/faces/twitter/tweetubhai/128.jpg
+did:ethr:0x62d939a3482894607a5e81785b94b754352d7fce  Brionna    Barrows     https://s3.amazonaws.com/uifaces/faces/twitter/mastermindesign/128.jpg
+did:ethr:0x7d2394864871dcc687d60be25bbec2b2bdd24be8  Keegan     White       https://s3.amazonaws.com/uifaces/faces/twitter/xiel/128.jpg
+did:ethr:0x893ca3d11a10c669c734947d55ecb73b119da855  Salvatore  Parisian    https://s3.amazonaws.com/uifaces/faces/twitter/scottgallant/128.jpg
+did:ethr:0x99ad5518cbefb0fd8b0367a6ca65ae39ca40a5f8  Dario      Hamill      https://s3.amazonaws.com/uifaces/faces/twitter/bpartridge/128.jpg
+did:ethr:0xa86b0fe00e4aac5663fd13767c4c95b7e5b086a9  Ursula     Murray      https://s3.amazonaws.com/uifaces/faces/twitter/ludwiczakpawel/128.jpg
+did:ethr:0xbb94e70f0d6c484cffb5f6c0733b00fbfbd890f2  Emmanuel   Dibbert     https://s3.amazonaws.com/uifaces/faces/twitter/ivanfilipovbg/128.jpg
+did:ethr:0xbcd3da8d436b3756d709dbc998d61e61be08e899  Fred       Hirthe      https://s3.amazonaws.com/uifaces/faces/twitter/sreejithexp/128.jpg
+did:ethr:0xca2235476fe2783f3c85ae7730a864c7f29a9752  Desmond    Osinski     https://s3.amazonaws.com/uifaces/faces/twitter/charliegann/128.jpg
+did:ethr:0xd2223c835c5b2d694e34fa33c0ce2ee7abe540b0  Elliott    Macejkovic  https://s3.amazonaws.com/uifaces/faces/twitter/VinThomas/128.jpg
+did:ethr:0xf3273078eb9167b0a8b6c029cafd7b8531d9ec40  Monica     Gusikowski  https://s3.amazonaws.com/uifaces/faces/twitter/kanickairaj/128.jpg
+```
--- generate.ts
+++ generate.ts
@@ -2,16 +2,15 @@ import program = require('commander')
 import EthrDID = require('ethr-did')
 import faker = require('faker')
 import sqlite3 = require('sqlite3')
+import cTable = require('console.table')
 
 program
   .option("--dids <n>", 'Number of dids', parseInt)
   .option("--db <s>", 'Database filename')
-  .option("--table <s>", 'Table name')
   .parse(process.argv)
 
 const numberOfDids = program.dids || 3
 const dbName = program.db || 'data.sqlite'
-const tableName = program.table || 'signed_data'
 
 declare interface Profile {
   did: any, // EthrDID object
@@ -25,6 +24,8 @@ const claimTypes = ['firstName', 'lastName', 'email', 'avatar']
 
 let users: Profile[] = []
 
+console.log('Creating identities...')
+
 for (let x = 0; x < numberOfDids; x++) {
   const keypair = EthrDID.createKeyPair()
   const did = new EthrDID({...keypair})
@@ -37,11 +38,15 @@ for (let x = 0; x < numberOfDids; x++) {
   })
 }
 
+console.log('Done.')
+
 let promises = []
 let unsigned = []
 
-// Every user signs all claims about all other users
 
+console.log('Signing claims...')
+
+// Every user signs all claims about all other users
 for (let x = 0; x < numberOfDids; x++) {
   for (let y = 0; y < numberOfDids; y++) {
     for (const claimType in claimTypes) {
@@ -60,15 +65,58 @@ for (let x = 0; x < numberOfDids; x++) {
   }
 }
 
+console.log('Done.')
+
+
 const db = new sqlite3.Database('./' + dbName)
 
 Promise.all(promises).then((jwts) => {
   // console.log(jwts)
 
   db.serialize(function() {
-    db.run(`CREATE TABLE IF NOT EXISTS ${tableName} (iss TEXT, sub TEXT, json_blob TEXT, raw TEXT)`)
-    const statement = db.prepare(`INSERT INTO ${tableName} VALUES (?,?,?,?)`)
-    
+    console.log('Creating tables and views')
+    db.run(`CREATE TABLE IF NOT EXISTS signed_data (iss TEXT, sub TEXT, json_blob TEXT, raw TEXT)`)
+    db.run(`CREATE INDEX "sub" ON "signed_data" ("sub");`)
+    db.run(`CREATE TABLE profile_data (parent_id INTEGER, iss TEXT, sub TEXT, claim_type TEXT, claim_value TEXT);`)
+    db.run(`CREATE INDEX "profile_data_sub" ON "profile_data" ("sub");`)
+    db.run(`CREATE INDEX "profile_data_claim_type" ON "profile_data" ("claim_type");`)
+    db.run(`CREATE TRIGGER insert_profile_data AFTER INSERT ON "signed_data" BEGIN INSERT INTO profile_data select new.rowid, a.iss, a.sub, b.key as claim_type, b.value as claim_value from signed_data a, json_tree(json_blob) b where b.path = '$.claim' and a.rowid = new.rowid; END;`)
+    db.run(`CREATE TRIGGER delete_profile_data BEFORE DELETE ON "signed_data" BEGIN DELETE FROM profile_data where parent_id = old.rowid; END;`)
+    db.run(`create view  popular_first_name as select * from (
+      select sub, claim_value as firstName
+      from "profile_data" 
+      where claim_type='firstName'
+      group by sub, claim_type, claim_value
+      order by count( claim_value) asc
+      ) group by sub`)
+
+      db.run(`create view  popular_last_name as select * from (
+        select sub, claim_value as lastName
+        from "profile_data" 
+        where claim_type='lastName'
+        group by sub, claim_type, claim_value
+        order by count( claim_value) asc
+      ) group by sub`)
+      db.run(`create view popular_avatar as select * from (
+        select sub, claim_value as avatar
+        from "profile_data" 
+        where claim_type='avatar'
+        group by sub, claim_type, claim_value
+        order by count( claim_value) asc
+      ) group by sub`)
+      db.run(`create view distinct_dids as select distinct sub from profile_data`)
+      db.run(`create view profiles as select distinct_dids.sub, popular_first_name.firstName, popular_last_name.lastName, popular_avatar.avatar
+      from distinct_dids
+      INNER join 
+        popular_first_name on popular_first_name.sub = distinct_dids.sub,
+        popular_last_name on popular_last_name.sub = distinct_dids.sub,
+        popular_avatar on popular_avatar.sub = distinct_dids.sub`)
+
+    console.log('Done.')
+    console.log('Inserting values...')
+
+    const statement = db.prepare(`INSERT INTO signed_data VALUES (?,?,?,?)`)
+
     for (const key in jwts) {
         const jwt = jwts[key]
         const obj = unsigned[key]
@@ -77,12 +125,19 @@ Promise.all(promises).then((jwts) => {
     }
 
     statement.finalize()
+    console.log('Done.')
 
-  });
+
+    db.all('SELECT count(*) FROM signed_data',[],(err, result) => {
+      console.log('\nSELECT count(*) FROM signed_data\n')
+      console.log(cTable.getTable(result))
+    } )
+    .all('SELECT * FROM profiles',[],(err, result) => {
+      console.log('SELECT * FROM profiles\n')
+      console.log(cTable.getTable(result))
+    } )
   
+  });
+
   db.close();
 })
-
-
-
-/// select a.sub, b.key, b.value from signed_data a, json_tree(json_blob) b where b.path = '$.claim';
